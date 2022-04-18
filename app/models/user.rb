bcf22a4ee354f21e03dfc60939c7fd542e4185c7
suzu1103 <4425krkh@gmail.com>
2022-04-18 15:33:50 +0000

    [Add]4-25 headerのレイアウトまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
-  
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -13,5 +14,5 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => "homes#about", as: "about"
   resources :books, only: [:new, :index, :show]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
+  
+  # 参考
+  # resources :users, only: [:show, :edit, :update]
+  # resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  #   resource :favorites, only: [:create, :destroy]
+  # resources :post_comments, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
