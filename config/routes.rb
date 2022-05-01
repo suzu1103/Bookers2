cab1d591976d5089fc0f4283b9c1c27cac0dc37a
suzu1103 <4425krkh@gmail.com>
2022-05-01 08:59:01 +0000

    [Add]bookの一覧と詳細のレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:index, :show, :edit]
-  
+  resources :users, only: [:index, :show, :edit,:update]
+
   # 参考
   # resources :users, only: [:show, :edit, :update]
   # resources :post_images, only: [:new, :create, :index, :show, :destroy] do
