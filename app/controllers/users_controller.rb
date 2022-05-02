cd0e6f3dedd81812eb678ba27dd5c978f6f16c17
suzu1103 <4425krkh@gmail.com>
2022-05-02 16:06:51 +0000

    [Add]4-25 ユーザーの投稿一覧の途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,27 @@ class UsersController < ApplicationController
     @users = User.all
     @books = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @book = Book.new
+    @books = @user.book
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
-  
+
+
   private
   def user_params
     params.require(:user).permit(:title, :introduction, :profile_image)
   end
-  
+
 end
