f52a90c9a7ba2ebb34e2a8d3d31fdbde35f522a8
suzu1103 <4425krkh@gmail.com>
2022-05-03 15:19:12 +0000

    [Add]4-25 本の編集ページの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @user = current_user.id
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
